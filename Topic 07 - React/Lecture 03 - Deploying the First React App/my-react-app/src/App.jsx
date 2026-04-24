52678cc3aece7faa072b48a94dafed4111ac1321
Shah Fahad <fahadluckyboy43@gmail.com>
2026-04-24 08:06:16 +0300

    first react app with vite

---
+++ Topic 07 - React/Lecture 03 - Deploying the First React App/my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello React with Vite</h1>
+      <p>This is my first working app.</p>
+      <div>
+        <p>I have learned the following things so far: </p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>TailwindCSS</li>
+          <li>JavaScript</li>
+          <li>Git</li>
+          <li>GitHub</li>
+          <li>TypeScript</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
