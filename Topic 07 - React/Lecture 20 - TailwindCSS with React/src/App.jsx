1b188e64c5b32e6d70d78eceeb854f6232842502
Shah Fahad <fahadluckyboy43@gmail.com>
2026-09-04 23:37:52 +0300

    using TailwindCSS

---
+++ Topic 07 - React/Lecture 20 - TailwindCSS with React/src/App.jsx
@@ -0,0 +1,378 @@
+import myimage from "./assets/images.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <div className="flex overflow-hidden flex-wrap justify-center">
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="max-w-70 m-10">
+          <div className="border flex flex-col items-center items gap-4 p-2 rounded">
+            <img className="rounded" src={myimage} alt="cardImg" />
+            <h2>About</h2>
+            <p>
+              I am professional in protecting your data from anyone confident
+              who beliefs who can get your data.{" "}
+            </p>
+            <button className="border p-1 rounded font-bold hover:cursor-pointer bg-gray-600">
+              Contact
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
